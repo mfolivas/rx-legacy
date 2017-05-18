d1c97ae15492fb141d539dafb7d1cd1da1907843
Marcelo Olivas <mfolivas@gmail.com>
2017-05-17 20:57:09 -0400

    Getting the unstable trading service working.

--- src/test/java/com/mfolivas/Exercises.java
+++ src/test/java/com/mfolivas/Exercises.java
@@ -2,8 +2,10 @@ package com.mfolivas;
 
 import com.mfolivas.finance.StockInfo;
 import com.mfolivas.finance.StockTradingService;
+import com.mfolivas.finance.UnstableStockTradingService;
 
 import org.junit.Test;
+import org.junit.internal.matchers.ThrowableCauseMatcher;
 
 import java.util.Arrays;
 import java.util.List;
@@ -37,6 +39,30 @@ public class Exercises {
         Observable.<StockInfo>create(emitter -> emitStock(emitter, STOCKS))
                 .takeWhile(symbol -> symbol.getPrice() > 30)
                 .forEach(System.out::println);
+    }
+
+    @Test
+    public void shouldSkipUntilPriceIsOver100() {
+        Observable.<StockInfo>create(emitter -> emitStock(emitter, STOCKS))
+                .skipWhile(stock -> stock.getPrice() > 100)
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void shouldCaptureExceptionWithUnstableTradingService() {
+        Observable.<StockInfo>create(emitter -> emitFreeStock(emitter, STOCKS))
+                .subscribe(
+                        System.out::println,
+                        System.out::println ,
+                        () -> System.out.println("DONE")
+                );
+    }
+
+    private static void emitFreeStock(ObservableEmitter<StockInfo> emitter, List<String> symbols) {
+        symbols.stream()
+                .map(stock -> new StockInfo(stock, UnstableStockTradingService.getPrice(stock)))
+                .forEach(emitter::onNext);
+        emitter.onComplete();
     }
 
     private static void emitStock(ObservableEmitter<StockInfo> emitter, List<String> symbols) {
